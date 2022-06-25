e43997b7051aadae83aed5fcd597972776368be9
taechnique <tnine923@gmail.com>
2022-06-25 14:50:28 +0900

    create subject for triple

---
+++ src/main/java/io/taech/triple/tripletravelersclub/TripleTravelersClubApplication.java
@@ -0,0 +1,13 @@
+package io.taech.triple.tripletravelersclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripleTravelersClubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripleTravelersClubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/taech/triple/tripletravelersclub/TripleTravelersClubApplicationTests.java
@@ -0,0 +1,13 @@
+package io.taech.triple.tripletravelersclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripleTravelersClubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
