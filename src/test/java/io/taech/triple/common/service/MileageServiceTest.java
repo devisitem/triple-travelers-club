279744507f6fc2fe2f18ad50ea83dfadf412f8d5
taechnique <tnine923@gmail.com>
2022-07-02 04:29:52 +0900

    add mockito-inline for static method test

--- src/test/java/io/taech/triple/common/service/MileageServiceTest.java
+++ src/test/java/io/taech/triple/common/service/MileageServiceTest.java
@@ -1,10 +1,7 @@
 package io.taech.triple.common.service;
 
 import io.taech.triple.business.events.constant.MileageUsage;
-import io.taech.triple.business.events.entity.QReviewRewardInfo;
-import io.taech.triple.business.events.entity.ReviewRewardInfo;
-import io.taech.triple.business.events.entity.TravelersReview;
-import io.taech.triple.business.events.entity.TripleUser;
+import io.taech.triple.business.events.entity.*;
 import io.taech.triple.business.events.repository.RewardInfoRepository;
 import io.taech.triple.common.base.MockingTester;
 import io.taech.triple.common.excpeted.EventProcessingException;
@@ -13,12 +10,12 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockedStatic;
 
 import java.sql.SQLClientInfoException;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 class MileageServiceTest extends MockingTester {
 
@@ -32,20 +29,29 @@ class MileageServiceTest extends MockingTester {
     @Test
     @DisplayName("[마일리지 적립 서비스] 정보 저장내 예외 발생시 기준에러 쓰로우")
     public void manageMileagesWithReview1() throws Throwable {
-        /* Given */
-        final TravelersReview review = mock(TravelersReview.class);
-        final TripleUser user = mock(TripleUser.class);
-        final MileageUsage mileageUsage = MileageUsage.ADD_WRITE_REVIEW;
-        final ReviewRewardInfo rewardInfo = mock(ReviewRewardInfo.class);
+        try (
+                MockedStatic<ReviewRewardInfo> info = mockStatic(ReviewRewardInfo.class);
+                MockedStatic<MileageHistory> history = mockStatic(MileageHistory.class)
+        ) {
+            /* Given */
+            final TravelersReview review = new TravelersReview();
+            final TripleUser user = new TripleUser();
+            final MileageUsage mileageUsage = MileageUsage.ADD_WRITE_REVIEW;
+            final ReviewRewardInfo rewardInfo = new ReviewRewardInfo();
+            final MileageHistory mileageHistory = new MileageHistory();
 
-        /* When */
-        when(rewardInfoRepository.save(rewardInfo))
-                .thenThrow(new RuntimeException("존재하지 않는 컬럼입니당."));
-        final EventProcessingException actual = assertThrows(EventProcessingException.class, () -> mileageService.manageMileagesWithReview(review, user, mileageUsage));
+            /* When */
+            when(MileageHistory.create(mileageUsage, user ))
+                    .thenReturn(mileageHistory);
+            when(ReviewRewardInfo.create(mileageUsage, review, mileageHistory))
+                    .thenReturn(rewardInfo);
+            when(rewardInfoRepository.save(rewardInfo))
+                    .thenThrow(new RuntimeException("존재하지 않는 컬럼입니당."));
+            final EventProcessingException actual = assertThrows(EventProcessingException.class, () -> mileageService.manageMileagesWithReview(review, user, mileageUsage));
 
-        /* Then */
-        assertEquals(ServiceStatus.FAILED_TO_MANAGE_MILEAGES, actual.getServiceStatus());
-
+            /* Then */
+            assertEquals(ServiceStatus.FAILED_TO_MANAGE_MILEAGES, actual.getServiceStatus());
+        }
     }
 
 }
